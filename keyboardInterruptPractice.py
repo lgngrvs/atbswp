c7f7c37e7539700905bf466a10a65da27bf3ced2
Logan Graves <github@logangraves.com>
2021-06-17 13:08:10 -0700

    Add hello world and keyboard interrupt programs

---
+++ keyboardInterruptPractice.py
@@ -0,0 +1,5 @@
+# Ctrl-C to keyboardInterrupt
+
+while True:
+    print('Hello, world!')
+    # I want to add wait() but I'm not sure if it's a thing I can do until the python package for vscode installs.
