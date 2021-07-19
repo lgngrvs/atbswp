cb35839dc392e9d21e32d639b354bff6d2bd260b
Logan Graves <github@logangraves.com>
2021-07-19 14:33:36 -0700

    move files into chapters1thru2 folder

---
+++ chapters1thru2/questionsAnswers.py
@@ -0,0 +1,20 @@
+# Boolean Values: True & false
+# Boolean Operators: and, or, not
+# AND: TrueTrue = true, everything else false
+# OR: FalseFalse = false, everything else true
+# NOT: true = false, false = true
+# Comparison operators: ==, !=, <, >, <=, >=
+spam = ''
+spam = str(spam)
+
+if spam == "1": 
+    print("hello")
+elif spam == "2": 
+    print("Howdy")
+else: 
+    print("Greetings!")
+
+# Ctrl-c
+# no difference
+# spam.bacon()
+# round() rounds, abs() just gives absolute value
